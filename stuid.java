9b4f2ffec60d73f8d5dcb2f350de41fcaccf4f64
Bhavya Aprajita <bhavyaaprajita30@gmail.com>
2017-03-24 18:04:51 +0530

    student id

---
+++ stuid.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class stuid {
+  public  static String id;
+   public static String email;
+   public static void setstuid(String i)
+    {
+   stuid.id=i;
+   
+    }
+  
+    
+}
